fae26c726c8f1a216db28fa289844ee054483545
LEVI2612 <103377836+CHHIEVTONGLY@users.noreply.github.com>
2024-08-21 16:07:26 +0700

    Axios updated also remove aba debug

--- server/routers/ABARouter.js
+++ server/routers/ABARouter.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const { ABACreateOrder } = require("../Controller/ABAController");
 
-console.log("ABACreateOrder:", ABACreateOrder); // Add this line
 router.post("/checkout", ABACreateOrder);
 
 module.exports = router;
